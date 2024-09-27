027c6f17a7ba9a5086c0d0e686f181f01504815c
ekrars2699 <ekrars@nycstudents.net>
2024-09-27 14:05:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Ekrar S."; // update the part BETWEEN the "quotes"
+let userName = "ekrars2699"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
